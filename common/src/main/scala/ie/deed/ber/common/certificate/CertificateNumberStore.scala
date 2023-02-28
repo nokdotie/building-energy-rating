226e6cbbaa5512c0554834c0d38d6263fe20bef9
Philippe Vinchon <p.vinchon@gmail.com>
2023-02-28 12:13:22 +0000

    Remove the need for memento

--- common/src/main/scala/ie/deed/ber/common/certificate/CertificateNumberStore.scala
+++ common/src/main/scala/ie/deed/ber/common/certificate/CertificateNumberStore.scala
@@ -4,45 +4,31 @@ import com.google.cloud.firestore._
 import zio._
 import zio.gcp.firestore.{CollectionPath, DocumentPath, Firestore}
 import scala.util.chaining.scalaUtilChainingOps
-import scala.collection.JavaConverters.{asScalaBufferConverter, mapAsJavaMapConverter, mapAsScalaMapConverter}
+import scala.collection.JavaConverters.{
+  asScalaBufferConverter,
+  mapAsJavaMapConverter,
+  mapAsScalaMapConverter
+}
 
 trait CertificateNumberStore {
-  val memento: ZIO[Any, Throwable, Option[CertificateNumber]]
-  def upsertBatch(certificateNumbers: Iterable[CertificateNumber]): ZIO[Any, Throwable, Unit]
+  def upsertBatch(
+      certificateNumbers: Iterable[CertificateNumber]
+  ): ZIO[Any, Throwable, Unit]
 }
 
 object CertificateNumberStore {
-  val memento: ZIO[CertificateNumberStore, Throwable, Option[CertificateNumber]] =
-    ZIO.serviceWithZIO[CertificateNumberStore](_.memento)
   def upsertBatch(
       certificateNumbers: Iterable[CertificateNumber]
   ): ZIO[CertificateNumberStore, Throwable, Unit] =
-    ZIO.serviceWithZIO[CertificateNumberStore](_.upsertBatch(certificateNumbers))
+    ZIO.serviceWithZIO[CertificateNumberStore](
+      _.upsertBatch(certificateNumbers)
+    )
 }
 
 class GoogleFirestoreCertificateNumberStore(
     firestore: Firestore.Service,
     collectionPath: CollectionPath
 ) extends CertificateNumberStore {
-  val certificateNumberField = "certificate-number"
-
-  val memento: ZIO[Any, Throwable, Option[CertificateNumber]] =
-    for {
-        collectionReference <- firestore.collection(collectionPath)
-        querySnapshot <- ZIO.fromFutureJava {
-            collectionReference
-                .orderBy(certificateNumberField, Query.Direction.DESCENDING)
-                .limit(1)
-                .get()
-        }
-        value = querySnapshot
-            .getDocuments
-            .asScala
-            .headOption
-            .flatMap { _.getData.asScala.get(certificateNumberField) }
-            .map { _.asInstanceOf[Long].toInt.pipe(CertificateNumber.apply) }
-    } yield value
-
   def upsertBatch(
       certificateNumbers: Iterable[CertificateNumber]
   ): ZIO[Any, Throwable, Unit] =
@@ -50,16 +36,17 @@ class GoogleFirestoreCertificateNumberStore(
       collectionReference <- firestore.collection(collectionPath)
       documentReferences <- certificateNumbers
         .map { certificateNumber =>
-          firestore.document(
-            collectionReference,
-            DocumentPath(certificateNumber.value.toString)
-          ).map { (certificateNumber, _) }
+          firestore
+            .document(
+              collectionReference,
+              DocumentPath(certificateNumber.value.toString)
+            )
+            .map { (certificateNumber, _) }
         }
         .pipe { ZIO.collectAll }
       writeBatch <- firestore.batch
       _ = documentReferences.foreach { (certificateNumber, documentReference) =>
-        val fields = Map(certificateNumberField -> certificateNumber.value)
-        writeBatch.set(documentReference, fields.asJava, SetOptions.merge)
+        writeBatch.set(documentReference, Map.empty.asJava, SetOptions.merge)
       }
       _ <- firestore.commit(writeBatch)
     } yield ()
