58f92612dab2a334dd27a8b2036eefc15250223e
Philippe Vinchon <p.vinchon@gmail.com>
2023-03-12 23:21:23 +0000

    Add endpoint to read from GCP

--- api/src/main/scala/ie/deed/ber/api/MainApp.scala
+++ api/src/main/scala/ie/deed/ber/api/MainApp.scala
@@ -1,10 +1,12 @@
 package ie.deed.ber.api
 
 import ie.deed.ber.common.dao.BERRecordInMemoryDao
+import ie.deed.ber.common.certificate.GoogleFirestoreCertificateStore
 import zio._
 import zio.http._
 import zio.http.model.Method
 import zio.http.middleware.Cors.CorsConfig
+import zio.gcp.firestore.Firestore
 
 object MainApp extends ZIOAppDefault {
 
@@ -17,14 +19,20 @@ object MainApp extends ZIOAppDefault {
   )
 
   private val routes =
-    TokenGenerationApp.apply() ++ BERApp(BERRecordInMemoryDao).http
+    apps.CertificateApp.http ++
+    TokenGenerationApp.apply()
 
   private val app =
     routes // @@ Middleware.debug @@ Middleware.cors(corsConfig) @@ Middleware.csrfGenerate()
 
   override val run = for {
     _ <- Console.printLine(s"Starting server on http://localhost:8080")
-    _ <- Server.serve(app).provide(Server.default)
+    _ <- Server.serve(app).provide(
+      Server.default,
+    Firestore.live,
+    GoogleFirestoreCertificateStore.layer,
+    Scope.default,
+    )
   } yield ()
 
 }
---
+++ api/src/main/scala/ie/deed/ber/api/apps/CertificateApp.scala
@@ -0,0 +1,23 @@
+package ie.deed.ber.api.apps
+
+import ie.deed.ber.common.certificate.{ CertificateNumber, CertificateStore }
+import scala.util.chaining.scalaUtilChainingOps
+import zio.ZIO
+import zio.http._
+import zio.http.model.{Method, Status}
+import zio.json._
+
+object CertificateApp {
+
+  val http: Http[CertificateStore, Nothing, Request, Response] = Http.collectZIO[Request] {
+    case Method.GET -> _ / "ber" / int(certificateNumber) =>
+        CertificateNumber(certificateNumber)
+            .pipe { CertificateStore.getById }
+            .fold(
+                _ => Response(Status.InternalServerError),
+            {
+                case None => Response(Status.NotFound)
+                case Some(certificate) => Response.json(certificate.toString)
+            })
+  }
+}
