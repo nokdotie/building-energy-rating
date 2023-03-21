87f983e7563d821d042eda6ab26baadaafe1bc88
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2023-03-21 00:17:59 +0000

    Small cleanup

--- api/src/main/scala/ie/deed/ber/api/MainApp.scala
+++ api/src/main/scala/ie/deed/ber/api/MainApp.scala
@@ -1,13 +1,16 @@
 package ie.deed.ber.api
 
+import ie.deed.ber.api.apps.{ApiV1CertificateApp, HealthApp}
 import ie.deed.ber.common.certificate.GoogleFirestoreCertificateStore
-import zio._
-import zio.http._
+import zio.*
+import zio.http.*
 import zio.http.model.Method
 import zio.http.middleware.Cors.CorsConfig
 import zio.gcp.firestore.Firestore
 
 object MainApp extends ZIOAppDefault {
+
+  private val port = 8080
 
   // CORS
   private val corsConfig: CorsConfig = CorsConfig(
@@ -18,26 +21,22 @@ object MainApp extends ZIOAppDefault {
   )
 
   private val routes =
-    apps.ApiV1CertificateApp.http ++
-      TokenGenerationApp.apply() ++
-      apps.HealthApp.http
+    ApiV1CertificateApp.http ++
+      HealthApp.http
 
-  private val app = (
-    routes @@ HttpAppMiddleware.debug @@ HttpAppMiddleware.cors(
-      corsConfig
-    ) // @@ HttpAppMiddleware.csrfGenerate()
-  ).withDefaultErrorResponse
+  private val app = (routes
+    @@ HttpAppMiddleware.debug
+    @@ HttpAppMiddleware.cors(corsConfig)).withDefaultErrorResponse
 
   override val run = for {
-    _ <- Console.printLine(s"Starting server on http://localhost:8080")
+    _ <- Console.printLine(s"Starting server on http://localhost:$port")
     _ <- Server
       .serve(app)
       .provide(
-        Server.default,
+        Server.defaultWithPort(port),
         Firestore.live,
         GoogleFirestoreCertificateStore.layer,
         Scope.default
       )
   } yield ()
-
 }
