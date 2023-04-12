d1f89315522954a55db9f8bc9baa6a5de8dcab86
Philippe Vinchon <p.vinchon@gmail.com>
2023-04-12 13:46:39 +0100

    Helper to restart stream

--- common/src/main/scala/ie/deed/ber/common/certificate/CertificateNumber.scala
+++ common/src/main/scala/ie/deed/ber/common/certificate/CertificateNumber.scala
@@ -10,15 +10,21 @@ object CertificateNumber {
   val MinValue: CertificateNumber = CertificateNumber(100_000_000)
   val MaxValue: CertificateNumber = CertificateNumber(110_000_000)
 
+  def streamAllFrom(start: Int): ZStream[Any, Throwable, CertificateNumber] = {
+    val startOrMin = Math.max(start, MinValue.value)
+    val startOrMax = Math.min(start, MaxValue.value)
+
+    val startToMax = ZStream.range(startOrMin, MaxValue.value)
+    val minToStart = ZStream.range(MinValue.value, startOrMax)
+
+    startToMax
+      .concat(minToStart)
+      .map(CertificateNumber.apply)
+  }
+
   val streamAllWithRandomStart: ZStream[Any, Throwable, CertificateNumber] =
     Random
       .nextIntBetween(MinValue.value, MaxValue.value)
       .pipe(ZStream.fromZIO)
-      .flatMap { mid =>
-        val midEnd = ZStream.range(mid, MaxValue.value)
-        val startMid = ZStream.range(MinValue.value, mid)
-
-        midEnd.concat(startMid)
-      }
-      .map(CertificateNumber.apply)
+      .flatMap(streamAllFrom)
 }
